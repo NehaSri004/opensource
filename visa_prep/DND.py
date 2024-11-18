20b384f12dee672fbb37537ceb7e64bff868f643
Neha Sri Nareddy <121238965+NehaSri004@users.noreply.github.com>
2024-11-18 14:25:55 +0530

    Create DND.py

---
+++ visa_prep/DND.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+num1 = num2 = 0
+for x in arr:
+    if x%m == 0:
+        num1 += x
+    else:
+        num2 += x
+print(num1-num2)
